9f95edce1648d1f4b69d6020b8e725062242f859
Ameer Hamza <ameerhamza03086940@gmail.com>
2025-07-31 12:47:39 +0500

    Add files via upload

---
+++ Personal Protfolio Website HTML, CSS & JavaScript/script.js
@@ -0,0 +1,56 @@
+/*=========================== Toggle icon navabr ===========================*/
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/*=========================== Scroll section Active link ===========================*/
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /*=========================== Stickey navbar ===========================*/
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /*=========================== Remove toggle icon and navbar when click navbar link (Scroll) ===========================*/
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+  /*=========================== Scroll reveal ===========================*/
+
+  ScrollReveal({ 
+    // reset: true, 
+    distance: '80px',
+    duration: 2000,
+    dilay: 200
+  });
+
+  ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+  ScrollReveal().reveal('.home-img, .services-container, .protfolio-box, .contact-form', { origin: 'bottom' });
+  ScrollReveal().reveal('.about-img, .home-content h1', { origin: 'left' });
+  ScrollReveal().reveal('.about-content, .home-content p', { origin: 'right' });
